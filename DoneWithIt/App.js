eda695d44aca8928508ea1229ddb893a244c6bd7
Homayoun <homayoun763@gmail.com>
2026-08-02 00:23:40 +0330

    Text

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView,Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function App() {
-  return ( 
+  const handlePress = () => console.log("Text pressed")
+  return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore expedita
+        aliquam ipsa eius laborum corrupti nisi, impedit perferendis soluta
+        quisquam aspernatur qui enim aut modi magnam eaque? Cum, perferendis
+        accusantium?
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -13,6 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue'
+    backgroundColor: "dodgerblue",
+    justifyContent: 'center',
+    alignContent : "center"
   },
 });
