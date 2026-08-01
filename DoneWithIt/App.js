62a420dc14d17539ad7fa92e21660a6bbe71f3a7
Homayoun <homayoun763@gmail.com>
2026-08-02 00:20:34 +0330

    View

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView,Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  return ( 
+    <SafeAreaView style={styles.container}>
       <Text>Hello World</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'dodgerblue'
   },
 });
