9b7badd5ba2fb47d1b6d945cffa0cc67a9bc99dd
Homayoun <homayoun763@gmail.com>
2026-08-03 00:22:03 +0330

    Flexbox- justifyContent, alignItems and alignSelf

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -6,13 +6,15 @@ export default function App() {
       style={{
         backgroundColor: "#fff",
         flex: 1,
-        flexDirection: "row",
+        flexDirection: "row", // horizontal
+        justifyContent : "center", // main
+        alignItems: "center" // secondary
       }}
     >
       <View
-        style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        style={{ backgroundColor: "dodgerblue", width: 100, height: 300 }}
       />
-      <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+      <View style={{ backgroundColor: "gold", width: 100, height: 200 }} />
       <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
     </View>
   );
