fe89fbf1569fdf695a41c7b39a19bbef99ca09da
Homayoun <homayoun763@gmail.com>
2026-08-02 19:18:44 +0330

    Button

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,36 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  TouchableWithoutFeedback,
-  View,
-} from "react-native";
+import { Button, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native</Text>
-      <TouchableWithoutFeedback
-        onPress={() => {
-          console.log("Image tapped");
-        }}
-      >
-        <Image
-          // blurRadius={10}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqB29nphbJsAHewsrwWTmt-rlpWHOlBvz5tEPVYDGnMA&s=10",
-          }}
-        />
-      </TouchableWithoutFeedback>
-      <TouchableNativeFeedback>
-        <View style={{ width: 200, height: 70, backgroundColor: "red" }}></View>
-      </TouchableNativeFeedback>
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() => console.log("Button tapped")}
+      />
     </View>
   );
 }
