388a8beeaa42347b14d1ced823299218e9b117d8
Homayoun <homayoun763@gmail.com>
2026-08-04 12:54:53 +0330

    Styling Text

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -9,32 +9,21 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View
+      <Text 
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10,
-          paddingLeft: 30,
+          // fontFamily: "Roboto",
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "bold",
+          color: "tomato",
+          textTransform: "capitalize",
+          textAlign : "center",
+          lineHeight : 30
         }}
       >
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 50,
-            height: 50,   
-          }}
-        ></View>
-      </View>
-      <View
-      style={{
-        backgroundColor: "tomato",
-        width : 100,
-        height : 100,
-        margin : 20  
-      }}
-      ></View>
+        I love React Native! This is my first React Native app! Here's some more
+        text
+      </Text>
     </View>
   );
 }
--- Note.md
+++ Note.md
@@ -144,3 +144,37 @@ export default function App() {
   );
 }
 ```
+
+## Styling Text
+
+```js 
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text 
+        style={{
+          // fontFamily: "Roboto",
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "bold",
+          color: "tomato",
+          textTransform: "capitalize",
+          textAlign : "center",
+          lineHeight : 30
+        }}
+      >
+        I love React Native! This is my first React Native app! Here's some more
+        text
+      </Text>
+    </View>
+  );
+}
+```
