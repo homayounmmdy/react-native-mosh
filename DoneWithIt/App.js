e7fed3bc7638e318c0edc874ffb8e9b6b29d8787
Homayoun <homayoun763@gmail.com>
2026-08-04 12:40:46 +0330

    Borders

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,5 +1,24 @@
-import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
 
 export default function App() {
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+          borderWidth: 10,
+          borderColor: "royalblue",
+          borderRadius: 50,
+        }}
+      ></View>
+    </View>
+  );
 }
--- DoneWithIt/app/screens/ViewImageScreen.js
+++ DoneWithIt/app/screens/ViewImageScreen.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     top: 40,
     left: 30,
     zIndex: 1,
-  },
+  }, 
   container: {
     backgroundColor: colors.black,
     flex: 1,
