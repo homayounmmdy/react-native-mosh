bdf6dee3b6155fd1d018167bc9e7c3cf69c901d2
Homayoun <homayoun763@gmail.com>
2026-08-08 19:53:47 +0330

    Extending the ListItem Component

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,10 +1,14 @@
 import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 import Screen from "./app/components/Screen";
 
 export default function App() {
   return (
     <Screen>
-      <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      <ListItem
+        title="My title"
+        ImageComponent={<Icon name="email" />}
+      />
     </Screen>
   );
 }
--- DoneWithIt/app/components/ListItem.js
+++ DoneWithIt/app/components/ListItem.js
@@ -3,15 +3,23 @@ import Swipeable from "react-native-gesture-handler/Swipeable";
 import colors from "../config/colors";
 import AppText from "./AppText";
 
-function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
+function ListItem({
+  title,
+  subTitle,
+  image,
+  ImageComponent,
+  onPress,
+  renderRightActions,
+}) {
   return (
     <Swipeable renderRightActions={renderRightActions}>
       <TouchableHighlight underlayColor={colors.light} onPress={onPress}>
         <View style={styles.container}>
-          <Image style={styles.image} source={image} />
-          <View>
+          {ImageComponent}
+          {image && <Image style={styles.image} source={image} />}
+          <View style={styles.detailContainer}>
             <AppText style={styles.title}>{title}</AppText>
-            <AppText style={styles.subTitle}>{subTitle}</AppText>
+            {subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText>}
           </View>
         </View>
       </TouchableHighlight>
@@ -21,14 +29,17 @@ function ListItem({ title, subTitle, image, onPress, renderRightActions }) {
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: "row",
+    flexDirection: " row",
     padding: 50,
+  },
+  detailContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   subTitle: {
     color: colors.medium,
