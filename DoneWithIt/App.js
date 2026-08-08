01b98a97b67bf2652416830054779348435d9c0b
Homayoun <homayoun763@gmail.com>
2026-08-08 20:56:11 +0330

    TextInput

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,5 +1,21 @@
-import ListingsScreen from "./app/screens/ListingsScreen";
+import { useState } from "react";
+import { Text, TextInput } from "react-native";
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <ListingsScreen  />;
+  const [firstName, setFirstName] = useState("");
+  return (
+    <Screen>
+      <Text>{firstName}</Text>
+      <TextInput
+      
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{
+          borderBlockColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      />
+    </Screen>
+  );
 }
