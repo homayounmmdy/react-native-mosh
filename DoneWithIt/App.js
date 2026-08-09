08993956f6abf2df6be40546ec2c50abd4ccd527
Homayoun <homayoun763@gmail.com>
2026-08-09 23:36:31 +0330

    Switch

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import AppTextInput from "./app/components/AppTextInput";
+import { Switch } from "react-native";
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [isNew, setIsNew] = useState(false);
   return (
     <Screen>
-      <AppTextInput placeholder="Username" icon="email" />
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
     </Screen>
   );
 }
