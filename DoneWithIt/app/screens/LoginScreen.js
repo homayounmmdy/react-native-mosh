b6b32f2a1ab53ddd10493a1a886d5a1d4935fe97
Homayoun <homayoun763@gmail.com>
2026-08-10 00:09:38 +0330

    Building the Login Form

---
+++ DoneWithIt/app/screens/LoginScreen.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { Image, StyleSheet } from "react-native";
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import Screen from "../components/Screen";
+
+function LoginScreen() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="email"
+        placeholder="email"
+        keyboardType="email-address"
+        onChangeText={(text) => setEmail(text)}
+        textContentType="emailAddress"
+      />
+      <AppTextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        icon="lock"
+        onChangeText={(text) => setPassword(text)}
+        placeholder="password"
+        secureTextEntry
+        textContentType="password"
+      />
+      <AppButton title="login" onPress={() => console.log(email, password)} />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+export default LoginScreen;
