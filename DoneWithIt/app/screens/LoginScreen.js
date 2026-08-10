fad38fec2457d05cd7a055b7ddf5b5b305484720
Homayoun <homayoun763@gmail.com>
2026-08-10 20:46:43 +0330

    The Touched State

--- DoneWithIt/app/screens/LoginScreen.js
+++ DoneWithIt/app/screens/LoginScreen.js
@@ -22,7 +22,7 @@ function LoginScreen() {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors , setFieldTouched , touched}) => (
           <>
             <AppTextInput
               autoCapitalize="none"
@@ -30,20 +30,22 @@ function LoginScreen() {
               icon="email"
               placeholder="email"
               keyboardType="email-address"
+              onBlur={() => setFieldTouched('email')}
               onChangeText={handleChange("email")}
               textContentType="emailAddress"
             />
-            <ErrorMessage error={errors.email} />
+            <ErrorMessage error={errors.email} visible={touched.email}/>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
               icon="lock"
               onChangeText={handleChange("password")}
+              onBlur={() => setFieldTouched('password')}
               placeholder="password"
               secureTextEntry
               textContentType="password"
             />
-            <ErrorMessage error={errors.password} />
+            <ErrorMessage error={errors.password} visible={touched.password}/>
             <AppButton title="login" onPress={handleSubmit} />
           </>
         )}
