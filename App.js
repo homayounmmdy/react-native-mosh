f6d53e98c4f710c70eadea0db777da49abbe9fc6
Homayoun <homayoun763@gmail.com>
2026-09-02 22:17:51 +0330

    Permissions Module

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
-
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import Screen from "./app/components/Screen";
 export default function App() {
-  return <ListingEditScreen />;
+  const requestPermission = async () => {
+    const {granted} = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+  useEffect(() => {
+    requestPermission();
+  }, []);
+  return <Screen></Screen>;
 }
