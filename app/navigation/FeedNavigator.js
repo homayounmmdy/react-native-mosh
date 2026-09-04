a4caf41ec768af528d8446d7fef05f6f25ee4f13
Homayoun <homayoun763@gmail.com>
2026-09-04 20:06:04 +0330

    Refactoring Routes

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -1,14 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import routes from "../navigation/routes";
 import ListingDetailsScreen from "../screens/ListingDetailsScreen";
 import ListingsScreen from "../screens/ListingsScreen";
-
 const Stack = createNativeStackNavigator();
 
 const FeedNavigator = () => (
   <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Listings" component={ListingsScreen} />
-    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
+    <Stack.Screen
+      name={routes.LISTING_DETAILS}
+      component={ListingDetailsScreen}
+    />
   </Stack.Navigator>
 );
 
-export default FeedNavigator; 
+export default FeedNavigator;
