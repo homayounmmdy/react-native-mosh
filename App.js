f2c5d8ad853535ffd42b91e538e6a73dfa9f021c
Homayoun <homayoun763@gmail.com>
2026-09-04 19:26:37 +0330

    Navigation Theme

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Text } from "react-native";
 import Screen from "./app/components/Screen";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import navigationTheme from "./app/navigation/navigationTheme";
 
 const Tweets = ({ navigation }) => (
   <Screen>
@@ -77,7 +78,7 @@ const TabNavigator = () => (
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
