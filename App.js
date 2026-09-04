547ac691b0517680e5cc232da8cb93af0904d2fd
Homayoun <homayoun763@gmail.com>
2026-09-04 17:53:52 +0330

    Setting Screen Titles

--- App.js
+++ App.js
@@ -1,33 +1,38 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, Text } from "react-native";
 import Screen from "./app/components/Screen";
 
-const Tweets = ({navigation}) => (
+const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
     <Button
-    title="View Tweet"
-    onPress={() => navigation.navigate('TweetDetails', {
-      id : 1,
-
-    })}  
+      title="View Tweet"
+      onPress={() =>
+        navigation.navigate("TweetDetails", {
+          id: 1,
+        })
+      }
     />
   </Screen>
 );
 
-const TweetDetails = ({route}) => (
+const TweetDetails = ({ route }) => (
   <Screen>
     <Text>Tweets Details {route.params.id}</Text>
   </Screen>
 );
 
-const Stack = createNativeStackNavigator(); 
+const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => (
   <Stack.Navigator>
     <Stack.Screen name="Tweets" component={Tweets} />
-    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen
+      name="TweetDetails"
+      component={TweetDetails}
+      options={({ route }) => ({ title: route.params.id })}
+    />
   </Stack.Navigator>
 );
 
