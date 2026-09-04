d65a454c31f84278a0b1abf0992875d2b04030f8
Homayoun <homayoun763@gmail.com>
2026-09-04 20:00:14 +0330

    Beautifying the Tabs

--- app/navigation/FeedNavigator.js
+++ app/navigation/FeedNavigator.js
@@ -5,16 +5,10 @@ import ListingsScreen from "../screens/ListingsScreen";
 const Stack = createNativeStackNavigator();
 
 const FeedNavigator = () => (
-  <Stack.Navigator mode="modal">
+  <Stack.Navigator mode="modal" screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Listings" component={ListingsScreen} />
-    <Stack.Screen
-      name="ListingDetails"
-      component={ListingDetailsScreen}
-      options={{
-        headerShown: false,
-      }}
-    />
+    <Stack.Screen name="ListingDetails" component={ListingDetailsScreen} />
   </Stack.Navigator>
 );
 
-export default FeedNavigator;
+export default FeedNavigator; 
