ff206ee396524d7ce6227377840fb3fa728df8d6
Homayoun <homayoun763@gmail.com>
2026-09-04 17:42:18 +0330

    Stack Navigator

--- App.js
+++ App.js
@@ -1,4 +1,33 @@
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import { Text } from "react-native";
+import Screen from "./app/components/Screen";
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweets Details</Text>
+  </Screen>
+);
+
+const Stack = createNativeStackNavigator(); 
+
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
+
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
