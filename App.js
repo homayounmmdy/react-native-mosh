c2fbfbcdbae984bb102454d0f92793d77bb86430
Homayoun <homayoun763@gmail.com>
2026-09-15 23:36:33 +0330

    AsyncStorage

--- App.js
+++ App.js
@@ -1,6 +1,18 @@
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
-  const netInfo = useNetInfo()
-  return null;
+  const demo = async () => {
+    try {
+      await AsyncStorage.setItem("person", JSON.stringify({ id: 1 }));
+      const value = await AsyncStorage.getItem("person");
+      const person = JSON.parse(value);
+      console.log(person);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  demo();
+
+  return <p>Hello</p>;
 }
