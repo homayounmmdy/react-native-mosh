c6ef19223ef16cf8ef87bf324c8f6b93d403f4a8
Homayoun <homayoun763@gmail.com>
2026-09-25 21:53:40 +0330

    Refactoring

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { AppLoading } from "expo";
-import jwtDecode from "jwt-decode";
 import { useState } from "react";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
@@ -12,14 +11,13 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState();
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
-    if (!token) return;
-    setUser(jwtDecode(token));
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
   };
   if (!isReady)
     return (
-      <AppLoading startAsync={restoreToken} onFinish={() => setIsReady(true)} />
+      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} />
     );
 
   return (
